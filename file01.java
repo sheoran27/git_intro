3c3fc7ca6165d14fc079aea5a265f04b172c1bf2
ysheoran <sheoranyogesh27@gmail.com>
2017-09-29 11:46:45 +0530

    greet by name

--- file01.java
+++ file01.java
@@ -1,5 +1,10 @@
 public class git1 {
 	public static void Main(String args) {
-       System.out.println("hello world");
+		greet("yogesh");
+
+	}
+
+	public static void greet(String name) {
+		System.out.println("hello world: " + name);
 	}
 }
